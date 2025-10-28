0eff90225d216746624556a8e116ae175c6a6c01
malishabandara <malishabandara2@gmail.com>
2025-10-28 20:29:04 +0530

    added a new button to the nav bar

--- client/components/layout/SiteHeader.tsx
+++ client/components/layout/SiteHeader.tsx
@@ -57,6 +57,15 @@ export const SiteHeader = () => {
         <div className="hidden items-center gap-3 lg:flex">
           <Button variant="outline" asChild>
             <a
+              href="https://tax.gov.ae/en/media.centre/news.aspx"
+              target="_blank"
+              rel="noreferrer"
+            >
+              FTA Updates
+            </a>
+          </Button>
+          <Button variant="outline" asChild>
+            <a
               href="https://wa.me/971556230447"
               target="_blank"
               rel="noreferrer"
